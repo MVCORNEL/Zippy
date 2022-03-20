051e049c1b16656d29814c946137f53849c8a8b5
ManeaValentinCornel <manea.valentin.cornel@gmail.com>
2022-03-20 13:31:26 +0000

    Sass/css prefixing, compressing, compiling + parcel build

--- src/js/models/trainingModel.js
+++ src/js/models/trainingModel.js
@@ -1,69 +1,79 @@
 class ModelTraining {
-    #trainings;
-    #currentElement;
+  #trainings;
+  #currentElement;
 
-    constructor() {
-        this.#currentElement=0;
-        this.#trainings = [
-            {
-                location: 'London',
-                role: 'Bartender',
-                description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.Delectus expedita culpa perferendis earumanimi repudiandae illo rem est harum nisi? Lorem ipsum dolor sit, amet consectetur ! ',
-                img: ["img-1", "img-2", "img-3"]
-            },
-            {
-                location: 'London',
-                role: 'Waiter',
-                description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.Delectus expedita culpa perferendis earumanimi repudiandae illo rem est harum nisi? Lorem ipsum dolor sit, amet consectetur !',
-                imgs: ["img-1", "img-2", "img-3"]
-            },
-            {
-                location: 'Londons',
-                role: 'CARPENTER 1',
-                description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.Delectus expedita culpa perferendis earumanimi repudiandae illo rem est harum nisi? Lorem ipsum dolor sit, amet consectetur ! ',
-                imgs: ["img-1", "img-2", "img-3"]
-            },
-            {
-                location: 'Londons',
-                role: 'CARPENTER 1',
-                description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.Delectus expedita culpa perferendis earumanimi repudiandae illo rem est harum nisi? Lorem ipsum dolor sit, amet consectetur ! ',
-                imgs: ["img-1", "img-2", "img-3"]
-            },
-        ];
-    }
+  constructor() {
+    this.#currentElement = 0;
+    this.#trainings = [
+      {
+        role: 'Bartender',
+        location: 'London',
+        rate: '10/14',
+        duration: '2 weeks',
+        date: '21 December',
+        description:
+          'Lorem ipsum dolor, sit amet consectetur adipisicing elit.Delectus expedita culpa perferendis earumanimi repudiandae illo rem est harum nisi? Lorem ipsum dolor sit, amet consectetur ! ',
+        img: ['img-1', 'img-2', 'img-3'],
+      },
+      {
+        role: 'Waiter',
+        location: 'London',
+        rate: '10/12',
+        duration: '3 days',
+        date: '19 December',
+        description:
+          'Lorem ipsum dolor, sit amet consectetur adipisicing elit.Delectus expedita culpa perferendis earumanimi repudiandae illo rem est harum nisi? Lorem ipsum dolor sit, amet consectetur !',
+        imgs: ['img-1', 'img-2', 'img-3'],
+      },
+      {
+        role: 'Barista',
+        location: 'London',
+        rate: '10/14',
+        duration: '4 days',
+        date: '1 January',
+        description:
+          'Lorem ipsum dolor, sit amet consectetur adipisicing elit.Delectus expedita culpa perferendis earumanimi repudiandae illo rem est harum nisi? Lorem ipsum dolor sit, amet consectetur ! ',
+        imgs: ['img-1', 'img-2', 'img-3'],
+      },
+      {
+        role: 'Receptionist',
+        location: 'London',
+        rate: '12/14',
+        duration: '1 week',
+        date: '3 September',
+        description:
+          'Lorem ipsum dolor, sit amet consectetur adipisicing elit.Delectus expedita culpa perferendis earumanimi repudiandae illo rem est harum nisi? Lorem ipsum dolor sit, amet consectetur ! ',
+        imgs: ['img-1', 'img-2', 'img-3'],
+      },
+    ];
+  }
 
-    get currentElement(){
-        return this.#currentElement;
-    }
+  get currentElement() {
+    return this.#currentElement;
+  }
 
-    get trainings(){
-        return this.#trainings;
-    }
+  get trainings() {
+    return this.#trainings;
+  }
 
-    nextTraining() {
-        this.#currentElement = (this.#currentElement + 1) % this.#trainings.length;
-        return this.currentElement;
-    }
-    
-    previousTraining() {
-        if (this.#currentElement === 0) {
-            this.#currentElement = this.#trainings.length - 1;
-        } else {
-            this.#currentElement--;
-        }
-        return this.#currentElement;
-    }
+  nextTraining() {
+    this.#currentElement = (this.#currentElement + 1) % this.#trainings.length;
+    return this.currentElement;
+  }
 
-    goTo(element){
-        this.#currentElement=element;
-        return this.#currentElement;
+  previousTraining() {
+    if (this.#currentElement === 0) {
+      this.#currentElement = this.#trainings.length - 1;
+    } else {
+      this.#currentElement--;
     }
+    return this.#currentElement;
+  }
 
+  goTo(element) {
+    this.#currentElement = element;
+    return this.#currentElement;
+  }
 }
 
-
 export default new ModelTraining();
-
-
-
-
--- src/js/views/trainingView.js
+++ src/js/views/trainingView.js
@@ -3,37 +3,34 @@ import img_2 from 'url:../../img/waiter-2.jpg';
 import img_3 from 'url:../../img/waiter-3.jpg';
 import View from './View';
 class TrainingView extends View {
+  //ALL THIS ELMENTS WILL BI INITIALIZED AFTER THE MARKUP WAS RENDER, THEY WON'T EXIST PRIOR TO THAT
+  _parentElement = document.querySelector('.cursor');
+  #btnNext;
+  #btnPrevious;
+  _errorMessage = 'There are not reviews to be displayed';
 
-    //ALL THIS ELMENTS WILL BI INITIALIZED AFTER THE MARKUP WAS RENDER, THEY WON'T EXIST PRIOR TO THAT
-    _parentElement = document.querySelector('.cursor');
-    #btnNext; 
-    #btnPrevious; 
-    _errorMessage = 'There are not reviews to be displayed';
-    
-    #trainings;
-    #dotsContainer;
-    #dots;
+  #trainings;
+  #dotsContainer;
+  #dots;
 
-
-    render(data,goTO=0) {
-        if(super.render(data)){
-            this.#initDOM();
-            this.#setResizeObserver();
-            this.goToElement(goTO);
-        }
+  render(data, goTO = 0) {
+    if (super.render(data)) {
+      this.#initDOM();
+      this.#setResizeObserver();
+      this.goToElement(goTO);
     }
+  }
 
-    #initDOM() {
-        this.#btnNext= document.querySelector('.cursor__controller-btn--next');
-        this.#btnPrevious= document.querySelector('.cursor__controller-btn--previous');
-        this.#dotsContainer = document.querySelector('.cursor__controller-dots');
-        this.#trainings = this._parentElement.querySelectorAll('.training');
-        this.#dots = this._parentElement.querySelectorAll('.dot');
-    }
+  #initDOM() {
+    this.#btnNext = document.querySelector('.cursor__controller-btn--next');
+    this.#btnPrevious = document.querySelector('.cursor__controller-btn--previous');
+    this.#dotsContainer = document.querySelector('.cursor__controller-dots');
+    this.#trainings = this._parentElement.querySelectorAll('.training');
+    this.#dots = this._parentElement.querySelectorAll('.dot');
+  }
 
-
-    _generateMarkup() {
-        return `
+  _generateMarkup() {
+    return `
         <div class="cursor__box">
         ${this.#generateMarkupTrainings()}
         </div>
@@ -44,11 +41,12 @@ class TrainingView extends View {
             </div>
             <button class="cursor__controller-btn cursor__controller-btn--next btn-arrow btn-arrow--active">&rarr;</button>
         </div>`;
-    }
+  }
 
-    #generateMarkupTrainings() {
-        return this._data.map((training, index) => {
-            return `
+  #generateMarkupTrainings() {
+    return this._data
+      .map((training, index) => {
+        return `
           
                 <div class="training" data-training="${index}">
                     <img class="training__image training__image--1" src="${img_1}" alt="picture waiters working">
@@ -60,62 +58,63 @@ class TrainingView extends View {
                         <h3 class="training__box-role heading-quertenary">${training.location}</h3>
                         <h4 class="training__box-location heading-tertiary"> ${training.role} </h4>
                         </div>
-                        <p class="training__box-description"> ${training.description} </p>
-                        <button class="btn training__box-btn">Find more!</button>
+                        <div>
+                            <p class="training__box-duration"><span class="text-bold">Duration: </span>${training.duration}</p>
+                            <p class="training__box-rate">
+                            <span class="text-bold">Earning potential : </span> ${training.rate}£ per hour
+                            </p>
+                            <p class="training__box-date"><span class="text-bold">Next training: </span> ${training.date}</p>
+                            <p class="training__box-description"> ${training.description} </p>
+                        </div>
+                       
+                        <button class="btn training__box-btn">Book now!</button>
                     </div>
                 </div>
           `;
-        }).join('');
-    }
+      })
+      .join('');
+  }
 
-    #generateMarkupDots() {
-        return this._data.map((_, index) =>
-                `<button class="dot" data-dot=${index}></button>`)
-            .join('');
-    }
+  #generateMarkupDots() {
+    return this._data.map((_, index) => `<button class="dot" data-dot=${index}></button>`).join('');
+  }
 
-    goToElement(goTo) {
-        this.#trainings.forEach((training, id) => {
-            training.style.transform = `translateX(${100*(id-goTo)}%)`;
-        });
-        this.#activateDots(goTo);
-    }
+  goToElement(goTo) {
+    this.#trainings.forEach((training, id) => {
+      training.style.transform = `translateX(${100 * (id - goTo)}%)`;
+    });
+    this.#activateDots(goTo);
+  }
 
+  #activateDots(activeDot) {
+    this.#dots.forEach((dot) => dot.classList.remove('dot--active'));
+    document.querySelector(`.dot[data-dot="${activeDot}"]`).classList.add('dot--active');
+  }
 
-    #activateDots(activeDot) {
-        this.#dots.forEach((dot) => dot.classList.remove('dot--active'));
-        document.querySelector(`.dot[data-dot="${activeDot}"]`).classList.add("dot--active");
-    }
+  #setResizeObserver() {
+    const highestElement = Array.from(this.#trainings).reduce((accumulator, current) =>
+      current.getBoundingClientRect().height > accumulator.getBoundingClientRect().height ? current : accumulator
+    );
+    const resizeObserver = new ResizeObserver((entries) => {
+      highestElement.parentElement.style.height = `${highestElement.getBoundingClientRect().height}px`;
+    });
+    resizeObserver.observe(highestElement);
+  }
 
-    #setResizeObserver() {
-        const highestElement = Array.from(this.#trainings).reduce((accumulator, current) =>
-            current.getBoundingClientRect().height > accumulator.getBoundingClientRect().height ? current : accumulator);
-        const resizeObserver = new ResizeObserver(entries => {
-            highestElement.parentElement.style.height = `${highestElement.getBoundingClientRect().height}px`;
-        });
-        resizeObserver.observe(highestElement);
-    }
+  addBtnsListeners(handlerPrevious, handlerNext) {
+    this.#btnPrevious?.addEventListener('click', handlerPrevious);
+    this.#btnNext?.addEventListener('click', handlerNext);
+  }
 
-    addBtnsListeners(handlerPrevious, handlerNext) {
-        this.#btnPrevious?.addEventListener('click', handlerPrevious);
-        this.#btnNext?.addEventListener('click', handlerNext);   
-    }
-
-
-    addDotsListeners(handler) {
-            this.#dotsContainer?.addEventListener('click', function (e) {
-                if (e.target.classList.contains('dot')) {
-                    const pressedDot = e.target;
-                    const dotNumber = pressedDot.dataset.dot;
-                    handler(dotNumber);
-                }
-            });
-        
-    }
-
-
-
+  addDotsListeners(handler) {
+    this.#dotsContainer?.addEventListener('click', function (e) {
+      if (e.target.classList.contains('dot')) {
+        const pressedDot = e.target;
+        const dotNumber = pressedDot.dataset.dot;
+        handler(dotNumber);
+      }
+    });
+  }
 }
-
 
 export default new TrainingView();
